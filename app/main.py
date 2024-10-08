902352732be09fcb63c70a7846390ec7ac62cba8
Felix Kunze <felix.kunze@t-online.de>
2024-10-08 12:07:04 +0200

    feat: track openapi.json in git repository

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from .api.v1.router import v1_router
+from pathlib import Path
+import json
 
 app = FastAPI(title="swncrew backend")
 
@@ -8,3 +10,14 @@ app.include_router(v1_router, prefix="/v1")
 @app.get("/")
 async def root():
     return {"message": "Hello World. This is the swncrew backend"}
+
+# Auto generate OpenAPI spec output
+openapi_output_path = Path("./openapi/openapi.json")
+# Ensure the output directory exists
+openapi_output_path.parent.mkdir(parents=True, exist_ok=True)
+
+# Auto generate OpenAPI spec output on startup
+@app.on_event("startup")
+async def generate_openapi():
+    openapi = app.openapi()
+    openapi_output_path.write_text(json.dumps(openapi, indent=2))
