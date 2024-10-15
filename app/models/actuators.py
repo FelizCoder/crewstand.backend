d565cb42536ca07a6322e1fad26e2ac66d456a23
Felix Kunze <felix.kunze@t-online.de>
2024-10-15 15:33:19 +0200

    feat(api): enhanced input validation
    - Added validation to ensure actuator IDs are within the valid range.
    - Updated OpenAPI specification to include additional constraints on the `id` field.
    - Added custom validation functions and error handling in the endpoints.
    - Added a new `ValidationError` model for more descriptive error messages.

--- app/models/actuators.py
+++ app/models/actuators.py
@@ -1,6 +1,6 @@
-from typing import Type
+from typing import Generic, List, Type, TypeVar
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class ActuatorEnum(str, Enum):
     SOLENOID = "solenoid valve"
@@ -9,7 +9,7 @@ class ActuatorEnum(str, Enum):
 
 class Actuator(BaseModel):
     type: ActuatorEnum
-    id: int
+    id: int = Field(..., ge=-1, examples=[0,1,2])
     
 class SolenoidValve(Actuator):
     type: ActuatorEnum = ActuatorEnum.SOLENOID
@@ -17,8 +17,23 @@ class SolenoidValve(Actuator):
     
 class ProportionalValve(Actuator):
     type: ActuatorEnum = ActuatorEnum.PROPORTIONAL
-    position: int
+    position: int = Field(..., ge=0, le=100)
     
 class Pump(Actuator):
     type: ActuatorEnum = ActuatorEnum.PUMP
     running: bool
+    
+T = TypeVar('T', bound=Actuator)
+
+class ActuatorRepository(Generic[T]):
+    item_type: T
+    count: int
+    
+    async def get_all(self) -> List[T]:
+        raise NotImplementedError
+    
+    async def get_by_id(self, actuator_id: int) -> T:
+        raise NotImplementedError
+    
+    async def set_state(self, actuator: T) -> T:
+        return NotImplementedError
