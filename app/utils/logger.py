cbd72f6809a413d6202cc37d1d2d6a39e19ea291
Felix Kunze <felix.kunze@t-online.de>
2024-10-15 12:04:43 +0200

    feat: enhanced logging
    - custom log messages follow format of fastapi
    - colored according log levels

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,6 +1,59 @@
-
 import logging
+
+class CustomFormatter(logging.Formatter):
+    """
+    This is a custom logging formatter, it allows to change how logs are displayed
+    """ 
+    # ANSI escape codes for formatting logs in different colors
+    grey = "\x1b[38;20m"
+    blue = '\x1b[38;5;39m'
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    # This is the basic format for logging messages
+    format = "%(levelname)s: %(message)s"
+
+    # This dictionary defines the formats for different log levels
+    FORMATS = {
+        logging.DEBUG: blue + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        """
+        This function formats a log message based on its level
+
+        Args:
+            record (LogRecord): The log record to be formatted.
+        Returns:
+            str: The formatted log message.
+        """
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+# Create the main logger
+logger = logging.getLogger()
+
+# Create a stream handler, which displays logs in the console
+ch = logging.StreamHandler()
+
+# Set the custom formatter for the stream handler
+ch.setFormatter(CustomFormatter())
+
+# Add the stream handler to the logger
+logger.addHandler(ch)
 
 
 def not_implemented_warning() -> None:
-    logging.warning("Not implemented yet")
+    """
+    A function to display a warning about a feature that's not implemented yet.
+
+    Returns:
+        None: This function does not return any value. It only logs a warning message.
+    """
+    logger.warning("Not implemented yet")
