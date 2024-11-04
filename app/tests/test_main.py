d2566dddf7748a23952cd8f3c9c70d459d58ae7f
Felix Kunze <felix.kunze@t-online.de>
2024-11-04 13:45:21 +0100

    test: Added test for Swagger UI
    issue #39

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -3,6 +3,7 @@
 from fastapi.testclient import TestClient
 
 from app.main import app
+from app.utils.config import settings
 
 client = TestClient(app)
 
@@ -11,3 +12,13 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World. This is the swncrew backend"}
+
+
+def test_swagger_ui_html():
+    # Test default behavior
+    response = client.get("/docs")
+    assert response.status_code == 200
+    assert "Swagger UI" in response.text
+    assert "openapi.json" in response.text
+    assert "/favicon.ico" in response.text
+    assert settings.PROJECT_NAME in response.text
