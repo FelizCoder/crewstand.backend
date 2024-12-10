9f9e4ae009645746ed81d5a84642271499ace21e
Felix Kunze <felix.kunze@t-online.de>
2024-12-10 14:39:58 +0100

    refactor(sensors): Refactor FlowmeterService to remove redundant methods
    * Removed get_all, get_by_id, and post_reading methods from FlowmeterService

--- app/services/sensors/flowmeter.py
+++ app/services/sensors/flowmeter.py
@@ -1,6 +1,6 @@
 # app/services/sensors/flowmeter.py
 from typing import List
-from app.models.sensors import Flowmeter, SensorReading
+from app.models.sensors import Flowmeter
 from app.repositories.sensors.flowmeter import FlowmeterSensor
 from app.services.sensors.service import SensorService
 
@@ -16,12 +16,3 @@ class FlowmeterService(SensorService[Flowmeter]):
     def __init__(self):
         sensor_repository = FlowmeterSensor()
         super().__init__(sensor_repository, Flowmeter)
-
-    def get_all(self) -> List[Flowmeter]:
-        return self.sensor.get_all()
-
-    def get_by_id(self, sensor_id: int) -> Flowmeter:
-        return self.sensor.get_by_id(sensor_id)
-
-    def post_reading(self, sensor_id: int, reading: SensorReading) -> Flowmeter:
-        return self.sensor.post_reading(sensor_id, reading)
