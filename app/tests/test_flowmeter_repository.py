dc313bd6a5894da407c5d5fa8835b957b8ca6368
Felix Kunze <felix.kunze@t-online.de>
2024-12-12 15:41:23 +0100

    chore: correct naming of test functions

--- app/tests/test_flowmeter_repository.py
+++ app/tests/test_flowmeter_repository.py
@@ -6,19 +6,20 @@ from app.repositories.sensors.flowmeter import FlowmeterSensor
 
 
 @pytest.fixture(name="repo")
-def solenoid_actuator():
+def flowmeter_repo():
     sensor = FlowmeterSensor()
     return sensor
 
-def test_proportional_actuator(repo):
+
+def test_flowmeter_sensor(repo):
     # Test get_all
     expected_flowmeters = [Flowmeter(id=0), Flowmeter(id=1)]
     repo.flowmeters = expected_flowmeters
-    
+
     flowmeters = repo.get_all()
     assert len(flowmeters) == len(expected_flowmeters)
     assert flowmeters == expected_flowmeters
-    
+
     # Test get_by_id
     flowmeter = repo.get_by_id(0)
     assert flowmeter == expected_flowmeters[0]
@@ -26,6 +27,6 @@ def test_proportional_actuator(repo):
     # Test set_state
     new_reading = SensorReading(timestamp_ns=1730906908814683100, value=42.42)
     expected = Flowmeter(id=0, current_reading=new_reading)
-    flowmeter = repo.post_reading(0,new_reading)
+    flowmeter = repo.post_reading(0, new_reading)
     assert repo.flowmeters[0] == expected
     assert flowmeter == expected
